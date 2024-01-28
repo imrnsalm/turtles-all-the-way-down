a45cb679b07fc26992eb72dd70efa3d4dd332c22
i8abyte <imransalim08@gmail.com>
2024-01-29 03:14:18 +1300

    Draw a pattern and upload its image

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 Drawing pictures with [Turtle graphics](https://docs.python.org/3/library/turtle.html) in Python.
 
 ---
-![pattern](/images/pattern.png)
+![pattern_00](/images/pattern_00.png)
+![pattern_01](/images/pattern_01.png)
 
--- turtles.py
+++ turtles.py
@@ -1,9 +1,26 @@
 from turtle import *
+from random import randrange
 
-for steps in range(100):
-    for tmnt in ('blue', 'red' ,'purple', 'orange'):
-        color(tmnt)
-        forward(steps)
-        left(50)
+tmnt = ['blue', 'red', 'purple', 'orange']
+
+# for steps in range(100):
+#     for t in tmnt:
+#         color(t)
+#         forward(steps)
+#         left(50)
+
+w = 79
+x = 1
+y = 2
+while x <= w:
+	t = tmnt[randrange(len(tmnt))]
+	color(t)
+	z = x + y
+	x = y
+	y = z
+	forward(x)
+	left(x)
+	back(y)
+	right(y)
 
 mainloop()
