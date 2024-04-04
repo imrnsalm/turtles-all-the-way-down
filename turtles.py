f92fdb082257ed2dcc1fddcaf8f66d64280b1c71
i8abyte <imransalim08@gmail.com>
2024-04-05 01:43:49 +1300

    Plan a plot of a map

--- turtles.py
+++ turtles.py
@@ -1,7 +1,7 @@
 from turtle import *
 from random import randrange
 
-tmnt = ['blue', 'red', 'purple', 'orange']
+# tmnt = ['blue', 'red', 'purple', 'orange']
 
 # for steps in range(100):
 #     for t in tmnt:
@@ -9,18 +9,35 @@ tmnt = ['blue', 'red', 'purple', 'orange']
 #         forward(steps)
 #         left(50)
 
-w = 79
-x = 1
-y = 2
-while x <= w:
-	t = tmnt[randrange(len(tmnt))]
-	color(t)
-	z = x + y
-	x = y
-	y = z
-	forward(x)
-	left(x)
-	back(y)
-	right(y)
+# t1 = Turtle()
+# t2 = Turtle()
+# t1.color(tmnt[0])
+# t2.color(tmnt[1])
+# w = 79
+# x = 1
+# y = 2
+# while x <= w:
+# 	z = x + y
+# 	x = y
+# 	y = z
 
-mainloop()
+# 	t1.fd(x)
+# 	t2.fd(y)
+
+# 	t1.lt(x)
+# 	t2.lt(x/2)
+
+# 	t1.bk(y)
+# 	t2.bk(x)
+
+# 	t1.rt(y)
+# 	t2.rt(y/2)
+
+'''
+ Plot a tinkerbell map.
+'''
+
+def chua_circuit():
+    mainloop()
+
+chua_circuit()
